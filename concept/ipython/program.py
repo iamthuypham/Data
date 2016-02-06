0834ca74908da8e9b0a8760b374b84bbc656ddc7
Thuy Pham <tpham045@hotmail.com>
2016-02-06 19:04:13 +0000

    Add python file dataStructure.py and program.py

---
+++ concept/ipython/program.py
@@ -0,0 +1,27 @@
+__author__ = 'tpham'
+#To change this template use Tools | Templates.
+
+print("First Challenge") 
+for i in 2,4,6,8:
+    print(i)
+print("Bonus Challenge")    
+for i in range(1,10):
+    print(i)
+print("Variables")    
+total =0
+for i in 1,3,7:
+    total = total +i
+print(total)
+print("Functions")  
+def say_hello_to(name):
+    print("Hello" + name)
+say_hello_to("Miranda")
+say_hello_to("Fred")
+print("Coditionals")  
+angle = 5
+if angle > 0:
+    print("one")
+elif if angle< 0:
+    print("two")
+else: 
+    print("three")
